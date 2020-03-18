aecd3de0d0c8d48f18c3ca9121c2d73636e975a3
Rick Henry <rickhenry@rickhenry.dev>
2020-03-18 13:14:30 -0400

    create all composers template with filter function
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- backend/app/composers/router.py
+++ backend/app/composers/router.py
@@ -7,11 +7,20 @@ from starlette.requests import Request
 from app.auth.models import UserInDB
 from app.auth.security import get_current_active_user
 from app.composers import crud
-from app.dependencies import get_next_prev_page_urls
+from app.dependencies import get_next_prev_page_urls, templates
 
 composer_router = APIRouter()
 
 logger = logging.getLogger()
+
+
+@composer_router.get("/{composer_name}")
+async def get_single_composer(
+    request: Request,
+    composer_name: str,
+    current_user: UserInDB = Depends(get_current_active_user),
+):
+    pass
 
 
 @composer_router.get("")
@@ -19,4 +28,7 @@ async def get_composers(
     request: Request, current_user: UserInDB = Depends(get_current_active_user),
 ):
     composers = await crud.get_all_composers(current_user.email)
-    return str(composers)
+    return templates.TemplateResponse(
+        "composers/all.html",
+        {"request": request, "composers": sorted(composers), "title": "All Composers"},
+    )
